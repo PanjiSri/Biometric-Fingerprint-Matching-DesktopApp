de98e9e76e66b65842c91dc6651e0fee4383ee45
Ibrahim Rasyid <thamrinihsan@gmail.com>
2024-05-23 14:21:59 +0700

    feat: program structure

---
+++ Tubes3_let-me-seedik/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tubes3_let_me_seedik
+{
+    internal static class Program 
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Regex r = new Regex();
+            Console.WriteLine(r.isMatch("hai", @"(ha)+."));
+            Console.WriteLine(r.isMatch("hahai", @"(ha)+."));
+            // Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new Form1());
+        }
+    }
+}
---
+++ Tubes3_let-me-seedik/Regex.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+
+public class Regex
+{
+    public bool isMatch(string text, string pattern)
+    {
+        return isMatchHelper(text, 0, "", pattern, 0);
+    }
+
+    private bool isMatchHelper(string text, int text_idx, string prefix, string pattern, int pattern_idx)
+    {
+        if (pattern_idx == pattern.Length)
+        {
+            return text_idx == text.Length;
+        }
+
+        bool firstMatch = (text_idx < text.Length && (pattern[pattern_idx] == text[text_idx] || pattern[pattern_idx] == '.'));
+
+        if (pattern[pattern_idx] == '(' )
+        {
+            pattern_idx++;
+            String sub = "";
+            while (pattern[pattern_idx] != ')' && pattern_idx < pattern.Length)
+            {
+                sub += pattern_idx;
+                pattern_idx++;
+            }
+        } 
+        else if (pattern[pattern_idx] == '[' )
+        {
+            pattern_idx++;
+            String sub = "";
+            while (pattern[pattern_idx] != ']' && pattern_idx < pattern.Length)
+            {
+                sub += pattern_idx;
+                pattern_idx++;
+            }
+        }
+
+        return true;
+    }
+
+    private bool checkAllSub(string text, string pattern)
+    {
+        return true;
+    }
+}
